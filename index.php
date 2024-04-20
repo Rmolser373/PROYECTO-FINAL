0c6f1f9fabe05cf7bf6e981a70f110c24661099c
Antonio Colmenero Callejón <acolcal203@g.educaand.es>
2024-04-20 11:50:23 +0200

    Sentencia en la base de datos desde PHP

--- index.php
+++ index.php
@@ -1,20 +1,33 @@
 <?php
+   
+  $nombreServidor = "localhost";
+  $baseDatos = "Ejemplo";
+  $nombreUsuario = "usuarioEjemplo";
+  $password = "usuarioEjemplo";
+  
+  // Se crea la conexión
+   
+  $conn = new mysqli($nombreServidor, $nombreUsuario, $password, $baseDatos);
+   
+  // Se comprueba que la conexión es correcta
+   
+  if (!$conn) {
     
-   
-   $nombreServidor = "localhost";
-   $baseDatos = "cooperativa_chocas";
-   $nombreUsuario = "usuario";
-   $password = "usuario";
-   
-   // Se crea la conexión
-   
-   $conn = new mysqli($nombreServidor, $nombreUsuario, $password, $baseDatos);
-   
-   // Se comprueba que la conexión es correcta
-   
-    if (!$conn) {
-       die("Connection failed: " . mysqli_connect_error());
-    }
+    die("Connection failed: " . mysqli_connect_error());
+  }
+  
+  echo "Conexión correcta";
+
+  $sentenciaSQL="INSERT INTO Localidad VALUES(3,'Córdoba')";
+ 
+  if (!$conn->query($sentenciaSQL)) {
+      
+    echo "Falló la inserción de datos en la tabla: (" . $conn->errno . ") " . $conn->error;
+  
+  }
     
-    mysqli_close($conn);
+  echo "Se ha ejecutado correctamente la inserción de datos";
+  
+  mysqli_close($conn);
+
 ?>
